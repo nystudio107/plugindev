696e4843319ad64cbf09d37c4289d335eda809fe
Andrew Welch <andrew@nystudio107.com>
2022-03-03 18:33:32 -0500

    refactor: Add to Rector config

--- cms_v3/rector.php
+++ cms_v3/rector.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use craft\rector\SetList as CraftSetList;
 use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -12,6 +14,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/vendor/nystudio107/craft-seomatic/src/integrations',
     ]);
 
+    // Craft Version
     $containerConfigurator->import(CraftSetList::CRAFT_CMS_40);
     $containerConfigurator->import(CraftSetList::CRAFT_COMMERCE_40);
+    // PHP Version
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+    // Other
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION);
 };
