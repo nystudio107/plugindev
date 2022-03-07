75e40cbee7fdba7b91d82947bc49127cbb3c009c
Andrew Welch <andrew@nystudio107.com>
2022-03-06 22:42:43 -0500

    feat: Enable parallel execution

--- cms_v4/ecs.php
+++ cms_v4/ecs.php
@@ -7,5 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PARALLEL, true);
     $containerConfigurator->import(CraftSetList::CRAFT_CMS_4);
 };
