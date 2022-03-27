588cc1719d33cb1dbfa473e24d2fd861c17bb742
Andrew Welch <andrew@nystudio107.com>
2022-03-26 20:59:21 -0400

    refactor: Run `composer craft-update` after a `composer install` is done via `composer_install.sh`

--- docker-config/php-prod-craft/composer_install.sh
+++ docker-config/php-prod-craft/composer_install.sh
@@ -12,5 +12,19 @@
 
 cd $CMS_ROOT_PATH
 if [ ! -f "composer.lock" ] || [ ! -d "vendor" ]; then
-    su-exec www-data composer install --verbose --no-progress --optimize-autoloader --no-interaction
+    su-exec www-data composer install --verbose --no-progress --no-scripts --optimize-autoloader --no-interaction
+    # Wait until the MySQL db container responds
+    echo "### Waiting for MySQL database"
+    until eval "mysql -h mysql -u $DB_USER -p$DB_PASSWORD $DB_DATABASE -e 'select 1' > /dev/null 2>&1"
+    do
+        sleep 1
+    done
+    # Wait until the Postgres db container responds
+    echo "### Waiting for Postgres database"
+    until eval "PGPASSWORD=$DB_PASSWORD psql -h postgres -U $DB_USER $DB_DATABASE -c 'select 1' > /dev/null 2>&1"
+    do
+        sleep 1
+    done
+    # Run any pending migrations/project config changes
+    su-exec www-data composer craft-update
 fi
