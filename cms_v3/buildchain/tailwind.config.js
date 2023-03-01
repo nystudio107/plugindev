fc3021f07f25c5d3c0e94053aacd52b7ff5ba011
Andrew Welch <andrew@nystudio107.com>
2023-03-01 09:44:39 -0500

    feat: Added an optional `buildchain` to both the `craft_v3` and `craft_v4` projects, which allows you to spin up a Vite buildchain; see the `Makefile` for available commands. This is for frontend testing.

---
+++ cms_v3/buildchain/tailwind.config.js
@@ -0,0 +1,10 @@
+// module exports
+module.exports = {
+  content: [
+    '../src/templates/**/*.{twig,html}',
+    './src/vue/**/*.{vue,html}',
+  ],
+  theme: {
+  },
+  plugins: [],
+};
---
+++ cms_v4/buildchain/src/js/app.ts
@@ -0,0 +1,4 @@
+// Import our CSS
+import '@/css/app.css';
+import '@/img/icon.svg'
+import '/public/img/icon.svg'
